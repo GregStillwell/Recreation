c29bfa7a1d81c2f12f362f5b10aa3be070ccd3c3
Greg <Jariye05@gmail.com>
2023-06-18 15:47:38 -0700

    added pElement and Pbody

--- js/script.js
+++ js/script.js
@@ -8,4 +8,10 @@ const bodyH1 = document.querySelector("body");
 bodyH1.append(h1Element);
 
 const pElement = document.createElement("p")
+pElement.append("The HTML of this webpage was created with JavaScript.")
+const pBody = document.querySelector("body");
+pBody.append(pElement);
+
+const imgElement = this.document.createElement("img");
+imgElement.append("https://cdn.download.ams.birds.cornell.edu/api/v1/asset/202984001/900")
 });
