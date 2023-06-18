ba9f796e78df8e2cc6e1d7a8f3098ab1b23940b3
Greg <Jariye05@gmail.com>
2023-06-18 15:41:47 -0700

    h1 remove and h1 header added

---
+++ js/script.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", function() {
+  const h1remove = document.querySelector("h1")
+  h1remove.remove();
+
+const h1Element = document.createElement("h1");
+h1Element.append("Webpage Recreation Practice");
+const bodyH1 = document.querySelector("body");
+bodyH1.append(h1Element);
+
+const pElement = document.createElement("p")
+});
